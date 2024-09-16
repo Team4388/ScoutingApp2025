9c254bb2491ddbe727556cdc6260bf4c51eb80ae
Astatin3 <77305074+Astatin3@users.noreply.github.com>
2024-09-15 23:15:13 -0600

    Fdroid - update versionName

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -20,8 +20,8 @@ android {
         applicationId = "com.ridgebotics.ridgescout"
         minSdk = 24
         targetSdk = 34
-        versionCode = 1
-        versionName = "1.0"
+        versionCode = 2
+        versionName = "0.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
