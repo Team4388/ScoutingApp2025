0a9f846e77bb73b2ecaec8bdcadd9e7cdecb2fea
Astatin3 <77305074+Astatin3@users.noreply.github.com>
2024-09-18 09:00:02 -0600

    Forgor to update version number

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -20,8 +20,8 @@ android {
         applicationId = "com.ridgebotics.ridgescout"
         minSdk = 24
         targetSdk = 34
-        versionCode = 2
-        versionName = "0.2"
+        versionCode = 3
+        versionName = "0.3"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
